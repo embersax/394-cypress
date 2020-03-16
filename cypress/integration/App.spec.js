b7d29c46b6dd6e7945eea31dfb5ddb211fa2cee7
embersax <ruilinzhou2022@u.northwestern.edu>
2020-03-16 16:53:57 -0500

    add interaction test

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -7,4 +7,10 @@ describe('Test App', () => {
         cy.visit('/');
         cy.get('[data-cy=banner]').should('be.visible');
     });
+
+    it('shows Google-sign in after clicking navbar', () => {
+        cy.visit('/');
+        cy.get('.navbar-burger').click();
+        cy.get('[data-cy=sign-in]').should('be.visible');
+    });
 });
